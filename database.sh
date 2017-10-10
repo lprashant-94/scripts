915298a7efed82e8733fcc20b6ff49558864a40a
Prashant Lokhande <prashant_lokhande@altizon.com>
2017-10-10 14:36:05 +0530

    Auto Suggest keys

--- database.sh
+++ database.sh
@@ -8,3 +8,22 @@ echo "$1,$2" >> ~/database
 db_get() {
 grep "^$1," ~/database | sed -e "s/^$1,//" | tail -n 1
 }
+
+
+_db_suggest() {
+local cur 
+local stringreply
+COMPREPLY=()
+cur=${COMP_WORDS[COMP_CWORD]}
+
+if (($COMP_CWORD == 1 ))
+then
+   stringreply=$(cat /home/prashant/database | sed -e "s/,.*//" | grep "$cur" | tr '\n' ' ' )
+   COMPREPLY=($stringreply)
+else
+    return 0
+fi
+}
+
+
+complete -F _db_suggest db_get 
